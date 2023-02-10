eb7689e2b5dd4dc61b2b7f800afab3e9a226814d
Lucas <08.lucas.sanchez@gmail.com>
2023-02-10 01:06:09 +0100

    ajout classe lorsque focus sur element

--- src/components/BoxElement.js
+++ src/components/BoxElement.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const BoxElement = ({ el, index }) => {
+    const [isFocused, setIsFocused] = useState(false);
+    const focusedElement = useRef()
+    const handleFocus = () => {
+        setIsFocused(true)
+        focusedElement.current.classList.add('is-focused');
+        console.log("handleFocus : ");
+        console.log(focusedElement.current.classList);
+    }
+    const handleBlur = () => {
+        setIsFocused(false)
+        focusedElement.current.classList.remove('is-focused');
+        console.log("handleBlur : ");
+        console.log(focusedElement.current.classList);
+    }
+    useEffect(() => {
+        focusedElement.current.addEventListener('focus', handleFocus);
+        focusedElement.current.addEventListener('blur', handleBlur);
+
+        return () => {
+            if (focusedElement.current) {
+                focusedElement.current.removeEventListener('focus', handleFocus);
+                focusedElement.current.removeEventListener('blur', handleBlur);
+            }
+        };
+
+    })
     return (
-        <div tabIndex={index + 1} className="boxElement">
+        <div ref={focusedElement} tabIndex={index + 1} className="boxElement">
             <a href={el.link}>
                 <h3>{el.title.length > 15 ? el.title.slice(0, 15).trimEnd() + "..." : el.title}</h3>
             </a>
